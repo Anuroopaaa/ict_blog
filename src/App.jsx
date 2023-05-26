b47d56aae1bd0b47c6ccc32502abb199ccc144f3
Anuroopa K S <anuroopa.saji@gmail.com>
2023-05-26 14:16:27 +0530

    blog1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+
+export default function App() {
+  return (
+    <div>
+      <Container>
+        <h1 className='d-flex justify-content-center mt-5'>BLOG</h1>
+
+
+      </Container>
+      
+
+    </div>
+  )
+}

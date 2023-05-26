fd4f0a889018bf42751f6f396a21d7a6025b6541
Anuroopa K S <anuroopa.saji@gmail.com>
2023-05-26 14:27:07 +0530

    blog2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
+import Nav1 from './Units/Nav1';
+import Home from './Pages/Home';
+import AddBlog from './Pages/AddBlog';
 
 export default function App() {
   return (
     <div>
       <Container>
+        <Nav1></Nav1>
         <h1 className='d-flex justify-content-center mt-5'>BLOG</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<AddBlog/>} />
+       </Routes>
 
 
       </Container>
--- src/Units/Nav1.jsx
+++ src/Units/Nav1.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Nav1() {
   return (
     <div>
         
+        <nav>
+       <ul className='d-flex justify-content-around list-unstyled mt-5 text-decoration-none'>
+          <li>
+             <Link to="/">Home</Link>
+          </li>
+          <li>
+             <Link to="/add">Add Blog</Link>
+          </li>
+         
+       </ul>
+ </nav>
 
-        
     </div>
   )
 }
